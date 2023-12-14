65a89bce1e7f5db8106f874caaf223237aa40b79
Raj S Tiwari <austiekbly@gmail.com>
2023-12-14 11:03:36 +0530

    model added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -2,6 +2,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
+import tensorflow as tf
 
 
 
@@ -13,16 +14,16 @@ class PredictionPipeline:
     
     def predict(self):
         # load model
-        model = load_model(os.path.join("model", "model.h5"))
+        model = load_model(os.path.join("model", "ct_scan_tumor_normal.h5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis=1)
+        result = model.predict(tf.expand_dims(test_image, axis=0))
+        result = int(tf.round(result))
         print(result)
 
-        if result[0] == 1:
+        if result == 1:
             prediction = 'Tumor'
             return [{ "image" : prediction}]
         else:
